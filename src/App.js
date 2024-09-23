1764819eb790e7931f6cdfeebf61d46700438fed
Lucas Gallo <lucasgallo@gmail.com>
2024-09-23 08:17:29 -0300

    testing components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { Navbar } from "./components/Navbar/Navbar";
+import Servicios from './components/Servicios/Servicios';
 
 function App() {
 
   const contenido = "Welcome"
-  const estilo = {display: "flex", justifyContent: "center", alignItems: "center", fontSize: "50px", width: "50%", border: "5px solid black"}
-  
+  const estilo = { display: "flex", justifyContent: "center", alignItems: "center", fontSize: "50px", width: "50%", border: "5px solid black" }
+
   return (
     <div>
       <Navbar />
       <p style={estilo}>{contenido}</p>
+      <Servicios />
     </div>
   );
 }
---
+++ src/components/Servicios/Servicios.js
@@ -0,0 +1,16 @@
+
+
+const Servicios = () => {
+
+    return (
+        <ul>
+            <h2>Servicios</h2>
+            <hr />
+            <li>Asistencia</li>
+            <li>Salud</li>
+            <li>Nutrición</li>
+        </ul>
+    )
+}
+
+export default Servicios
