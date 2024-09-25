5d0ca2b2f7d4600df73c0b4aa5ba272699444a06
Lucas Gallo <lucasgallo@gmail.com>
2024-09-24 21:10:19 -0300

    time

--- src/App.js
+++ src/App.js
@@ -9,16 +9,13 @@ import { ItemListContainer } from './components/ItemListContainer/ItemListContai
 function App() {
 
   const profesor = {
-    nombre: "Mike Dane",
+    nombre: "John Dane",
     rol: "Profesor de React",
     curso: "React"
   }
 
-  const tutor = {
-    nombre: "Francis Mallman",
-    rol: "Tutor de React"
-  }
-
+  const clock = () => <div>The time is {new Date().toLocaleTimeString()}</div>
+  
   const contenido = "Welcome"
   /* const estilo = { display: "flex", justifyContent: "center", alignItems: "center", fontSize: "50px", width: "50%", border: "5px solid black" } */
 
@@ -27,9 +24,10 @@ function App() {
       {/* <Navbar /> */}
       <Navbar2 />
       {/* <p style={estilo}>{contenido}</p> */}
+      <p className='clock'>{clock()}</p>
       <p className='welcome'>{contenido}</p>
-      <ItemListContainer nombre={profesor.nombre}/>
-      <Nosotros titulo={"Hi " +profesor.nombre} content={"Este es el curso de " + profesor.curso} />
+      <ItemListContainer nombre={profesor.nombre} />
+      <Nosotros titulo={"Hi " + profesor.nombre} content={"Este es el curso de " + profesor.curso} />
       <Nosotros titulo="Hi everybody" content="Este es el contenido" />
       <Nosotros />
       <Servicios />
