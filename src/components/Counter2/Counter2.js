281a3c8560477c9ed75a0d188aa88295d2945382
Lucas Gallo <lucasgallo@gmail.com>
2024-10-03 18:30:08 -0300

    useState

--- src/components/Counter2/Counter2.js
+++ src/components/Counter2/Counter2.js
@@ -3,28 +3,49 @@ import './Counter2.scss'
 
 export const Counter2 = () => {
 
+    const max = 10
+
     let [contador1, setContador1] = useState(0)
-    let [contador2, setContador2] = useState(0)
+    let [contador2, setContador2] = useState(10)
 
     // OPTION 1
-    const incrementar1 = () => {
-        setContador1(contador1 + 1)
+    const incrementar = () => {
+        if (contador1 < max) {
+            setContador1(contador1 + 1)
+        } else {
+            setContador1(contador1)
+            alert("Out of stock")
+        }
     }
-    const incrementar2 = () => {
-        setContador2(contador2 + 1)
-    }
+
+/*     const decrementar = () => {
+        if (contador2 > 0) {
+            setContador2(contador2 - 1)
+        } else {
+            setContador2(contador2)
+            alert("Out of stock")
+        }
+    } */
+
+    const decrementar = () => {
+        contador2 > 0 ? setContador2(contador2 - 1) : setContador2(contador2)
+        }
 
     return (
         <div className='counter2'>
             <h2>Counter</h2>
             <h5>Count independently</h5>
             <hr />
-            <p onClick={incrementar1}>{contador1}</p>
+            <button className="btn btn-primary" onClick={incrementar}>CLICK ME</button>
+            <h4>{contador1}</h4>
+            <p>Last click: {new Date().toLocaleString()}</p>
             <hr />
-            <p onClick={incrementar2}>{contador2}</p>
+            <button className="btn btn-primary" onClick={decrementar}>CLICK ME</button>
+            <h4>{contador2}</h4>
+            <p>Last click: {new Date().toLocaleString()}</p>
         </div>
     );
-
+}
 
     // OPTION 2
     /*     return (
@@ -40,6 +61,5 @@ export const Counter2 = () => {
                 <p>{contador2}</p>
             </div>
         ); */
-};
 
 export default Counter2;
