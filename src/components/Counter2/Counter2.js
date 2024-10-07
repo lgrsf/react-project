fe4d5dc2c2f21cd1799661f1d06e755fe4d5fa92
Lucas Gallo <lucasgallo@gmail.com>
2024-10-07 10:20:15 -0300

    render props

--- src/components/Counter2/Counter2.js
+++ src/components/Counter2/Counter2.js
@@ -3,61 +3,89 @@ import './Counter2.scss'
 
 export const Counter2 = () => {
 
-    const max = 10
+    const stock = 10
 
-    let [contador1, setContador1] = useState(0)
+    /* let [contador1, setContador1] = useState(0) */
+
+    const [contador1, setContador1] = useState({
+        contador1: 0,
+        date: new Date(),
+        stock: stock
+    })
+
+
     let [contador2, setContador2] = useState(10)
 
     // OPTION 1
+    /*     const incrementar = () => {
+            if (contador1 < 10) {
+                setContador1({
+                    contador1: contador1.contador1 + 1,
+                    date: new Date()
+                })
+            } else {
+                setContador1(contador1)
+                alert("Out of stock")
+            }
+        } */
+
     const incrementar = () => {
-        if (contador1 < max) {
-            setContador1(contador1 + 1)
+        if (contador1.stock > 0) {
+            setContador1({
+                contador1: contador1.contador1 + 1,
+                date: new Date(),
+                stock: contador1.stock - 1
+            })
         } else {
-            setContador1(contador1)
-            alert("Out of stock")
+            
+            alert("Outf of stock")
         }
     }
 
-    const decrementar = () => {
-        if (contador2 > 0) {
-            setContador2(contador2 - 1)
-        } else {
-            setContador2(contador2)
-            alert("Out of stock")
-        }
+
+const decrementar = () => {
+    if (contador2 > 0) {
+        setContador2(contador2 - 1)
+    } else {
+        setContador2(contador2)
+        alert("Out of stock")
     }
+}
 
 /*     const decrementar = () => {
         contador2 > 0 ? setContador2(contador2 - 1) : setContador2(contador2)
         } */
 
-    return (
-        <div className='counter2'>
-            <h2>Counter</h2>
-            <h5>Count independently</h5>
-            <hr />
-            <button className="btn btn-primary" onClick={incrementar}>+</button>
-            <h4>{contador1}</h4>
-            <hr />
-            <button className="btn btn-primary" onClick={decrementar}>-</button>
-            <h4>{contador2}</h4>
-        </div>
-    );
+return (
+    <div className='counter2'>
+        <h2>Counter</h2>
+        <h5>Count independently</h5>
+        <hr />
+        <button className="btn btn-primary" onClick={incrementar}>+</button>
+        <h4>{contador1.contador1}</h4>
+        <p>Stock: {contador1.stock}</p>
+        <p>Last click: {contador1.date.toLocaleString()}</p>
+        <hr />
+        <button className="btn btn-primary" onClick={decrementar}>-</button>
+        <h4>{contador2}</h4>
+    </div>
+);
+
 }
 
-    // OPTION 2
-    /*     return (
-            <div className="counter2">
-                <p onClick={() => setContador1(contador1 + 1)}></p>
-                <h2>Counter 1</h2>
-                <hr />
-                <p>{contador1}</p>
-    
-                <p onClick={() => setContador2(contador2 + 1)}></p>
-                <h2>Counter 2</h2>
-                <hr />
-                <p>{contador2}</p>
-            </div>
-        ); */
+// OPTION 2
+/*     return (
+        <div className="counter2">
+            <p onClick={() => setContador1(contador1 + 1)}></p>
+            <h2>Counter 1</h2>
+            <hr />
+            <p>{contador1}</p>
+ 
+            <p onClick={() => setContador2(contador2 + 1)}></p>
+            <h2>Counter 2</h2>
+            <hr />
+            <p>{contador2}</p>
+        </div>
+    ); */
 
 export default Counter2;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -6,10 +6,11 @@ export const ItemListContainer = ({ nombre, curso }) => {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    fontSize: "50px",
+    fontSize: "25px",
     width: "50%",
     border: "5px solid black",
     margin: "50px auto",
+    padding: "20px 0 30px 0",
   };
 
   return (
@@ -18,7 +19,13 @@ export const ItemListContainer = ({ nombre, curso }) => {
         <p>Welcome <br/>{nombre} <br/>{curso}</p>
         <Button
           variant="success"
-          onMouseEnter={() => (document.getElementById("demo").innerHTML = "Logged")}
+          onClick={() => (document.getElementById("demo").innerHTML = 
+          `
+          <div style={}>
+          <Button>Log out</Button>
+          </div>
+          ` 
+          )}
         >
           Log in
         </Button>
