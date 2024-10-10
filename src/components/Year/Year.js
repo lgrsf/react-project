8d174d0e4b59d956b1208b5da39c8d7aebd35095
Alejandro Lucas Gallo <lucas.gallo@gruposancristobal.com>
2024-10-10 15:15:00 -0300

    year

--- src/components/Year/Year.js
+++ src/components/Year/Year.js
@@ -4,8 +4,17 @@ import "./Year.scss";
 export const Year = () => {
   let [year, setYear] = useState(2024);
 
+  let x = 0;
+
   return (
-    <p className="year" onClick={() => setYear(year + 1)}>{year}</p>
+    <div>
+      <p className="year" onClick={() => setYear(year + 1)}>
+        {year}
+      </p>
+      <p id="demo" className="demo" onMouseMove={() => document.getElementById("demo").innerHTML = x+=1}>
+        Mouse over me!
+      </p>
+    </div>
   );
 };
 
